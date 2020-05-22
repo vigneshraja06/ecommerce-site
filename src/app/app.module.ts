fcf5d8049df396022f87e050f723d882738c5e40
vigneshraja06 <vigneshrajamlr@gmail.com>
2020-05-22 21:14:15 +0530

    ui commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
+import {HttpClientModule} from "@angular/common/http";
+import {NgxSpinnerModule} from "ngx-spinner";
+import {ToastrModule} from "ngx-toastr";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    CheckoutComponent,
+    HomeComponent,
+    ProductComponent,
+    ThankyouComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {ProductModelServer, serverResponse} from "../../models/product.model";
+import {Router} from "@angular/router";
+import {CartService} from "../../services/cart.service";
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  products: ProductModelServer[] = [];
+
+  constructor(private productService: ProductService,
+              private cartService: CartService,
+              private router:Router) {
+  }
+
+  ngOnInit() {
+    this.productService.getAllProducts(8).subscribe((prods: serverResponse ) => {
+      this.products = prods.products;
+      console.log(this.products);
+    });
+  }
+
+  AddProduct(id: Number) {
+    this.cartService.AddProductToCart(id);
+  }
+
+  selectProduct(id: Number) {
+    this.router.navigate(['/product', id]).then();
+  }
+}
